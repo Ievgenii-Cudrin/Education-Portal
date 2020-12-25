062df82023d61d2d34d29c399d0e219ea3420c8b
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2020-12-25 14:33:50 +0200

    Create user context

---
+++ DataAccessLayer/DataContext/UserContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccessLayer.Entities;
+
+namespace DataAccessLayer.DataContext
+{
+    public class UserContext
+    {
+        public List<User> Users { get; set; }
+    }
+}
