3ac20a57e08cf9333f2250102cc0651fe3dfb532
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2020-12-25 13:40:24 +0200

    Test commit

---
+++ ConsoleApplicationEP/ConsoleApplicationEP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplicationEP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
