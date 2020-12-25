63c619d8824b7f1823459fef06199fd7fa147780
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2020-12-25 13:58:33 +0200

    Delete unnecessary files, add consoleApp

---
+++ EducationPortalConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EducationPortalConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
