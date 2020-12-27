e60542785f81dc5a9728100da556c5ff4643f89a
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2020-12-27 16:53:53 +0200

    Get valid email

--- EducationPortalConsoleApp/Services/ProgramService.cs
+++ EducationPortalConsoleApp/Services/ProgramService.cs
@@ -3,6 +3,7 @@ using DataAccessLayer.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace EducationPortalConsoleApp.Services
 {
@@ -67,17 +68,69 @@ namespace EducationPortalConsoleApp.Services
                 $"\n4.Delete user");
             }
 
-            User GetDataFromUser()
+            User GenerateObjectFromUser()
             {
-                Console.WriteLine($"Enter your name: ");
-                string name = Console.ReadLine();
+                return new User()
+                {
+                    Name = GetNameFromUser(),
+                    Email = GetEmailFromUser(),
+                    PhoneNumber = ""
+                };
 
                 Console.WriteLine($"Enter your email adress: ");
                 string email = Console.ReadLine();
 
                 Console.WriteLine($"Enter your phone number: ");
                 string phoneNumber = Console.ReadLine();
+
+                return null;
             }
+
+            string GetNameFromUser()
+            {
+                EnterName:
+                Console.WriteLine($"Enter your name: ");
+                string name = Console.ReadLine();
+
+                if (String.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("Name must not be empty. Please, try again!");
+                    goto EnterName;
+                }
+                return name;
+            }
+
+            string GetEmailFromUser()
+            {
+                EnterEmail:
+                Console.WriteLine($"Enter your name: ");
+                string email = Console.ReadLine();
+
+                //check this expression
+                if (Regex.IsMatch(email, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase))
+                {
+                    Console.WriteLine("Incorrect email. Please, try again!");
+                    goto EnterEmail;
+                }
+                return email;
+            }
+
+            string GetPhoneNumber()
+            {
+                EnterPhoneNumber:
+                Console.WriteLine($"Enter your name: ");
+                string phoneNumber = Console.ReadLine();
+
+                if (Regex.IsMatch(phoneNumber, @"^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$", RegexOptions.IgnoreCase))
+                {
+                    Console.WriteLine("Incorrect phone number. Please, try again!");
+                    goto EnterPhoneNumber;
+                }
+                return phoneNumber;
+            }
+            
+
+
         }
     }
 }
