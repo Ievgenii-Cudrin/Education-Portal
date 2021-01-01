7c5b10b1ff20c9da0246138c1835befd79c2561b
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-01-01 22:44:50 +0200

    Create new entity - Book

---
+++ DataAccessLayer/Entities/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace DataAccessLayer.Entities
+{
+    [XmlType("Book")]
+    public class Book : Material
+    {
+        [XmlElement("CountOfPages")]
+        public int CountOfPages { get; set; }
+
+        [XmlElement("Author")]
+        public string Author { get; set; }
+    }
+}
