695374cfc7dbc83484042fc5118ad893168c2716
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-01-01 23:30:04 +0200

    Create new entity - Course

---
+++ DataAccessLayer/Entities/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace DataAccessLayer.Entities
+{
+    [XmlInclude(typeof(Skill)), XmlInclude(typeof(Material))]
+    public class Course
+    {
+        [XmlAttribute("MaterialID", DataType = "string")]
+        public string Id { get; set; }
+
+        [XmlElement("Description")]
+        public string Description { get; set; }
+
+        [XmlArray("SkillArray")]
+        [XmlArrayItem("SkillObjekt")]
+        public List<Skill> Skills = new List<Skill>();
+
+        [XmlArray("MaterialArray")]
+        [XmlArrayItem("MaterialObjekt")]
+        public List<Material> Materials = new List<Material>();
+    }
+}
