b2ccb6ba3c0025b0645394f2451a4f6e22d7a458
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-01-04 00:34:55 +0200

    Add  text work with one entity

---
+++ EducationPortalConsoleApp/Helpers/ProgramServiceConsoleMessageHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationPortalConsoleApp.Helpers
+{
+    public static class ProgramServiceConsoleMessageHelper
+    {
+        public static void ShowTextWithEntityToSelect()
+        {
+            Console.WriteLine($"Hi, dear user. Please, make your choice: " +
+            $"\n1.Operation with users" +
+            $"\n2.Operation with materials" +
+            $"\n3.Operation with courses" +
+            $"\n4.Operation with skills");
+        }
+    }
+}
--- EducationPortalConsoleApp/Services/ProgramService.cs
+++ EducationPortalConsoleApp/Services/ProgramService.cs
@@ -4,7 +4,31 @@ using System.Text;
 
 namespace EducationPortalConsoleApp.Services
 {
-    public class ProgramService
+    public static class ProgramService
     {
+        public static void SelectEntityToWork()
+        {
+            //TODO Console - 
+            string userChoice = Console.ReadLine();
+
+            switch (userChoice)
+            {
+                case "1":
+                    CreateMaterial();
+                    break;
+                case "2":
+                    UpdateMaterial();
+                    break;
+                case "3":
+                    ShowAllMaterials();
+                    break;
+                case "4":
+                    DeleteMaterial();
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+        }
     }
 }
