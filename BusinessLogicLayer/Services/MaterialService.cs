06333aba56c2b3bb8f2f26a01abb1f9d4d2adeea
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-01-15 00:52:30 +0200

    delete unit of work

--- BusinessLogicLayer/Services/MaterialService.cs
+++ BusinessLogicLayer/Services/MaterialService.cs
@@ -9,14 +9,8 @@ using System.Text;
 
 namespace EducationPortalConsoleApp.Services
 {
-    public class MaterialService : IMaterialService
+    public class MaterialService// : IMaterialService
     {
-        IUnitOfWork uow;
-        Material newMaterial;
-        public MaterialService()
-        {
-            this.uow = new EFUnitOfWork();
-        }
 
         
 
@@ -40,63 +34,63 @@ namespace EducationPortalConsoleApp.Services
         //    }
         //}
 
-        void UpdateMaterial()
-        {
-            //TODO finish this method
-            Console.Write($"Enter material ID to update: ");
-            int id = Convert.ToInt32(Console.ReadLine());
+        //void UpdateMaterial()
+        //{
+        //    //TODO finish this method
+        //    Console.Write($"Enter material ID to update: ");
+        //    int id = Convert.ToInt32(Console.ReadLine());
 
-            Material material = uow.Materials.Get(id);
-            if (material == null)
-            {
-                Console.WriteLine($"Material not found");
-            }
-            else
-            {
-                //TODO (Lisskov principe)
-                if (material is Video)
-                    material = VideoInstanceCreator.VideoCreator();
-                else if (material is Article)
-                    material = ArticleInstanceCreator.ArticleCreator();
-                else
-                    material = BookInstanceCreator.BookCreator();
+        //    Material material = uow.Materials.Get(id);
+        //    if (material == null)
+        //    {
+        //        Console.WriteLine($"Material not found");
+        //    }
+        //    else
+        //    {
+        //        //TODO (Lisskov principe)
+        //        if (material is Video)
+        //            material = VideoInstanceCreator.VideoCreator();
+        //        else if (material is Article)
+        //            material = ArticleInstanceCreator.ArticleCreator();
+        //        else
+        //            material = BookInstanceCreator.BookCreator();
 
-                material.Id = id;
-                uow.Materials.Update(material);
-                Console.WriteLine("Material has been successfully updated");
-            }
-        }
+        //        material.Id = id;
+        //        uow.Materials.Update(material);
+        //        Console.WriteLine("Material has been successfully updated");
+        //    }
+        //}
 
-        void ShowAllMaterials()
-        {
-            IEnumerable<Material> materials = uow.Materials.GetAll();
-            foreach(var material in materials)
-            {
-                //if (material is Video)
-                    //MaterialConsoleMessageHelper.ShowVideoInfo(material);
-                //else if (material is Article)
-                    //MaterialConsoleMessageHelper.ShowArticleInfo(material);
-                //else
-                    //MaterialConsoleMessageHelper.ShowBookInfo(material);
-            }
-            Console.WriteLine("\n");
-            //MaterialConsoleMessageHelper.ShowObjects(users);
+        //void ShowAllMaterials()
+        //{
+        //    IEnumerable<Material> materials = uow.Materials.GetAll();
+        //    foreach(var material in materials)
+        //    {
+        //        //if (material is Video)
+        //            //MaterialConsoleMessageHelper.ShowVideoInfo(material);
+        //        //else if (material is Article)
+        //            //MaterialConsoleMessageHelper.ShowArticleInfo(material);
+        //        //else
+        //            //MaterialConsoleMessageHelper.ShowBookInfo(material);
+        //    }
+        //    Console.WriteLine("\n");
+        //    //MaterialConsoleMessageHelper.ShowObjects(users);
 
-        }
+        //}
 
-        public bool Delete(int id)
-        {
-            Material material = uow.Materials.Get(id);
-            if (material == null)
-            {
-                return false;
-            }
-            else
-            {
-                uow.Materials.Delete(Convert.ToInt32(material.Id));
-            }
+        //public bool Delete(int id)
+        //{
+        //    Material material = uow.Materials.Get(id);
+        //    if (material == null)
+        //    {
+        //        return false;
+        //    }
+        //    else
+        //    {
+        //        uow.Materials.Delete(Convert.ToInt32(material.Id));
+        //    }
 
-            return true;
-        }
+        //    return true;
+        //}
     }
 }
