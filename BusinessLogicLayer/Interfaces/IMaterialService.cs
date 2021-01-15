bea356b570a5b9a1b07ec81ceca684e329bd72b8
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-01-15 11:57:12 +0200

    update IMaterialService

--- BusinessLogicLayer/Interfaces/IMaterialService.cs
+++ BusinessLogicLayer/Interfaces/IMaterialService.cs
@@ -6,5 +6,18 @@ namespace BusinessLogicLayer.Interfaces
 {
     public interface IMaterialService : IDeleteEntity
     {
+        public bool CreateVideo(string name, string link, int quality, int duration);
+
+        public bool CreateArticle(string name, string site, DateTime publicationDate);
+
+        public bool CreateBook(string name, string author, int countOfPages);
+
+        public bool UpdateVideo(int id, string name, string link, int quality, int duration);
+
+        public bool UpdateArticle(int id, string name, string site, DateTime publicationDate);
+
+        public bool UpdateBook(int id, string name, string author, int countOfPages);
+
+        public IEnumerable<string> GetAllMaterials();
     }
 }
