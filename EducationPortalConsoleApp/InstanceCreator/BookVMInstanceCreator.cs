3d6470dd6cc6a86dd7baa32572bca900d9fc86da
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-01-17 18:24:04 +0200

    add instances creators

---
+++ EducationPortalConsoleApp/InstanceCreator/BookVMInstanceCreator.cs
@@ -0,0 +1,23 @@
+﻿using EducationPortal.PL.Models;
+using EducationPortalConsoleApp.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationPortal.PL.InstanceCreator
+{
+    public static class BookVMInstanceCreator
+    {
+        public static BookViewModel CreateBook()
+        {
+            BookViewModel book = new BookViewModel()
+            {
+                Name = GetDataHelper.GetNameFromUser(),
+                Author = GetDataHelper.GetAuthorNameFromUser(),
+                CountOfPages = GetDataHelper.GetCountOfBookPages()
+            };
+
+            return book;
+        }
+    }
+}
