07524532bbff72e31abada36bd69b320c15e0e0a
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-14 20:16:58 +0200

    Migration CreateMaterial

--- EducationPortal.DAL.SQL/Migrations/20210214181622_CreateMaterial.Designer.cs
+++ EducationPortal.DAL.SQL/Migrations/20210214181622_CreateMaterial.Designer.cs
@@ -3,14 +3,16 @@ using EducationPortal.DAL.SQL.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EducationPortal.DAL.SQL.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20210214181622_CreateMaterial")]
+    partial class CreateMaterial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
