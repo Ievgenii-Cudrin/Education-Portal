600281a7c899eeef2f37a0d45bf23976bcaf5942
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-14 15:39:53 +0200

    Create application context

---
+++ EducationPortal.DAL.SQL/DataCOntext/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationPortal.DAL.SQL.DataCOntext
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Skill> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-7QBD7T4;Database=EducationPortal;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            
+        }
+    }
+}
