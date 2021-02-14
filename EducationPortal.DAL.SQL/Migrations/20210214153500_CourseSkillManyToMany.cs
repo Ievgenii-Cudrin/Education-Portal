d2490c0b220b5e05b75df09760df0847e0acc347
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-14 17:40:58 +0200

    Create SkillCourse ManyToMany

---
+++ EducationPortal.DAL.SQL/Migrations/20210214153500_CourseSkillManyToMany.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EducationPortal.DAL.SQL.Migrations
+{
+    public partial class CourseSkillManyToMany : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CourseSkills",
+                columns: table => new
+                {
+                    CourseId = table.Column<int>(type: "int", nullable: false),
+                    SkillId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseSkills", x => new { x.CourseId, x.SkillId });
+                    table.ForeignKey(
+                        name: "FK_CourseSkills_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CourseSkills_Skills_SkillId",
+                        column: x => x.SkillId,
+                        principalTable: "Skills",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseSkills_SkillId",
+                table: "CourseSkills",
+                column: "SkillId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CourseSkills");
+        }
+    }
+}
