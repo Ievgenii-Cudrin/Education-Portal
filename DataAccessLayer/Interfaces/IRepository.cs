bfafe90e84e06262c844f0ce2aee3598cef51b9c
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-15 23:49:29 +0200

    Update

---
+++ DataAccessLayer/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace DataAccessLayer.Interfaces
+{
+    using System.Collections.Generic;
+
+    public interface IRepository<T>
+        where T : class
+    {
+        public IEnumerable<T> GetAll();
+
+        public T Get(int id);
+
+        public void Create(T item);
+
+        public void Update(T item);
+
+        public void Delete(int id);
+    }
+}
---
+++ EducationPortalConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using EducationPortalConsoleApp.Branch;
+using EducationPortalConsoleApp.Helpers;
+using EducationPortalConsoleApp.Interfaces;
+using EducationPortalConsoleApp.Services;
+using Microsoft.Extensions.DependencyInjection;
+using EducationPortalConsoleApp.DependencyInjection;
+using System;
+
+namespace EducationPortalConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            ProgramBranch.StartApplication();
+
+            Console.ReadLine();
+        }
+    }
+}
