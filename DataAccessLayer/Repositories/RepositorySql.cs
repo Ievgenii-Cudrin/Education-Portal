7dc8c69d66f104c6091d41e958aea6070801ddb0
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-16 22:08:09 +0200

    Update RepositorySql.cs

--- DataAccessLayer/Repositories/RepositorySql.cs
+++ DataAccessLayer/Repositories/RepositorySql.cs
@@ -1,113 +1,131 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Text;
-
-namespace EducationPortal.DAL.Repositories
+﻿namespace EducationPortal.DAL.Repositories
 {
-    //public class RepositorySql
-    //{
-    //    private readonly DbContext context;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using DataAccessLayer.Interfaces;
+    using EducationPortal.DAL.DataContext;
+    using EducationPortal.Domain.Entities;
+    using Microsoft.EntityFrameworkCore;
 
-    //    public Repository(DbContext context)
-    //    {
-    //        this.context = context;
-    //    }
+    public class RepositorySql<T> : IRepository<T>
+        where T : class
+    {
+        private readonly ApplicationContext dbContext;
 
-    //    public IList<TEntity> GetAll<TEntity>()
-    //    {
-    //        return this.context.Set<TEntity>().ToList();
-    //    }
+        public RepositorySql(ApplicationContext context)
+        {
+            this.dbContext = context;
+        }
 
-    //    public IList<TEntity> GetAll<TEntity>(
-    //        params Expresion<Func<TEntity, TJoin>>[] includes)
-    //    {
-    //        var result = this.context.Set<TEntity>();
-    //        foreach (var include in includes)
-    //        {
-    //            result.Include(include);
-    //        }
+        public IList<T> GetAll()
+        {
+            return this.dbContext.Set<T>().ToList();
+        }
 
-    //        return result.ToList();
-    //    }
+        public IList<T> GetAll(params Expression<Func<T, object>>[] includes)
+        {
+            var result = this.dbContext.Set<T>();
+            foreach (var include in includes)
+            {
+                result.Include(include);
+            }
 
-    //    public IList<TEntity> Get<TEntity>(
-    //        Expresion<Func<TEntity, bool>> predicat,
-    //        params Expresion<Func<TEntity, TJoin>>[] includes)
-    //    {
-    //        var result = this.context.Set<TEntity>();
-    //        foreach (var include in includes)
-    //        {
-    //            result.Include(include);
-    //        }
+            return result.ToList();
+        }
 
-    //        return result.Where(predicat).ToList();
-    //    }
+        public IList<T> Get(
+            Expression<Func<T, bool>> predicat,
+            params Expression<Func<T, object>>[] includes)
+        {
+            var result = this.dbContext.Set<T>();
+            foreach (var include in includes)
+            {
+                result.Include(include);
+            }
 
-    //    public IList<TResult> Get<TEntity, TResult>(
-    //        Expresion<Func<TEntity, TResult>> selector)
-    //    {
-    //        return this.context.Set<TEntity>()
-    //                    .Select(selector).ToList();
-    //    }
+            return result.Where(predicat).ToList();
+        }
 
-    //    public IList<TResult> Get<TEntity, TResult>(
-    //        Expresion<Func<TEntity, TResult>> selector,
-    //        Expresion<Func<TEntity, bool>> predicat)
-    //    {
-    //        return this.context.Set<TEntity>()
-    //                    .Where(predicat)
-    //                    .Select(selector).ToList();
-    //    }
+        public IList<TResult> Get<TResult>(Expression<Func<T, TResult>> selector)
+        {
+            return this.dbContext.Set<T>()
+                        .Select(selector).ToList();
+        }
 
-    //    public IList<TEntity> GetPage<TEntity>(PageInfo page)
-    //    {
-    //        var skip = page.Size * (page.Number - 1);
-    //        return this.context.Set<TEntity>()
-    //                    .Skip(skip).Take(page.Size).ToList();
-    //    }
+        public IList<TResult> Get<TResult>(
+            Expression<Func<T, TResult>> selector,
+            Expression<Func<T, bool>> predicat)
+        {
+            return this.dbContext.Set<T>()
+                        .Where(predicat)
+                        .Select(selector).ToList();
+        }
 
-    //    public IList<TEntity> GetPage<TEntity>(
-    //        Expresion<Func<TEntity, bool>> predicat,
-    //        PageInfo page)
-    //    {
-    //        var skip = page.Size * (page.Number - 1);
-    //        return this.context.Set<TEntity>()
-    //                    .Where(predicat)
-    //                    .Skip(skip).Take(page.Size).ToList();
-    //    }
+        public IList<T> GetPage(PageInfo page)
+        {
+            var skip = page.Size * (page.Number - 1);
+            return this.dbContext.Set<T>()
+                        .Skip(skip).Take(page.Size).ToList();
+        }
 
-    //    public IList<TEntity> GetPage<TEntity>(
-    //        Expression<Func<TEntity, TResult>> selector,
-    //        Expression<Func<TEntity, bool>> predicat,
-    //        PageInfo page)
-    //    {
-    //        var skip = page.Size * (page.Number - 1);
-    //        return this.context.Set<TEntity>()
-    //                    .Select(selector)
-    //                    .Where(predicat)
-    //                    .Skip(skip).Take(page.Size).ToList();
-    //    }
+        public IList<T> GetPage(
+            Expression<Func<T, bool>> predicat,
+            PageInfo page)
+        {
+            var skip = page.Size * (page.Number - 1);
+            return this.dbContext.Set<T>()
+                        .Where(predicat)
+                        .Skip(skip).Take(page.Size).ToList();
+        }
 
-    //    public bool Exist<TEntity>(Expression<Func<TEntity, bool>> predicat)
-    //    {
-    //        return this.context.Set<TEntity>().Any(predicat);
-    //    }
+        // public IList<TEntity> GetPage<TEntity>(
+        //    Expression<Func<TEntity, TResult>> selector,
+        //    Expression<Func<TEntity, bool>> predicat,
+        //    PageInfo page)
+        //    where TEntity : class
+        // {
+        //    var skip = page.Size * (page.Number - 1);
+        //    return this.dbContext.Set<TEntity>()
+        //                .Select(selector)
+        //                .Where(predicat)
+        //                .Skip(skip).Take(page.Size).ToList();
+        // }
+        public bool Exist(Expression<Func<T, bool>> predicat)
+        {
+            return this.dbContext.Set<T>().Any(predicat);
+        }
 
-    //    public int AddAndSave<TEntity>(TEntity entity)
-    //    {
-    //        this.context.Set<TEntity>().Add(entity);
-    //        this.Save();
-    //    }
+        public void Add(T entity)
+        {
+            this.dbContext.Set<T>().Add(entity);
+        }
 
-    //    public int Add<TEntity>(TEntity entity)
-    //    {
-    //        this.context.Set<TEntity>().Add(entity);
-    //    }
+        public void Save()
+        {
+            this.dbContext.SaveChanges();
+        }
 
-    //    public void Save()
-    //    {
-    //        this.context.SaveChanges();
-    //    }
-    //}
+        public T Get(int id)
+        {
+            return this.dbContext.Set<T>().Find(id);
+        }
+
+        public void Update(T item)
+        {
+            this.dbContext.Entry<T>(item).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            var entity = this.dbContext.Set<T>().Find(id);
+
+            if (entity != null)
+            {
+
+                this.dbContext.Set<T>().Remove(entity);
+            }
+        }
+    }
 }
