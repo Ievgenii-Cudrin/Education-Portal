1cce0fc675f43bf2c4b4ed092ae32a67e11f4e83
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-16 22:07:22 +0200

    Update IRepository.cs

--- DataAccessLayer/Interfaces/IRepository.cs
+++ DataAccessLayer/Interfaces/IRepository.cs
@@ -1,18 +1,43 @@
 ﻿namespace DataAccessLayer.Interfaces
 {
+    using EducationPortal.Domain.Entities;
+    using System;
     using System.Collections.Generic;
+    using System.Linq.Expressions;
 
     public interface IRepository<T>
         where T : class
     {
-        public IEnumerable<T> GetAll();
+        public IList<T> GetAll();
 
         public T Get(int id);
 
-        public void Create(T item);
+        public void Add(T item);
 
         public void Update(T item);
 
         public void Delete(int id);
+
+        public IList<T> GetAll(params Expression<Func<T, object>>[] includes);
+
+        public void Save();
+
+        public bool Exist(Expression<Func<T, bool>> predicat);
+
+        public IList<T> GetPage(
+            Expression<Func<T, bool>> predicat,
+            PageInfo page);
+
+        public IList<T> GetPage(PageInfo page);
+
+        public IList<TResult> Get<TResult>(
+            Expression<Func<T, TResult>> selector,
+            Expression<Func<T, bool>> predicat);
+
+        public IList<TResult> Get<TResult>(Expression<Func<T, TResult>> selector);
+
+        public IList<T> Get(
+            Expression<Func<T, bool>> predicat,
+            params Expression<Func<T, object>>[] includes);
     }
 }
