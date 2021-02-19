8d9ee8034deb35a6dde7a3230706bd006f46641b
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-19 10:04:56 +0200

    Create services for many to many

---
+++ BusinessLogicLayer/ServicesSql/CourseMaterialSqlService.cs
@@ -0,0 +1,52 @@
+﻿namespace EducationPortal.BLL.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BusinessLogicLayer.Services;
+    using DataAccessLayer.Entities;
+    using DataAccessLayer.Interfaces;
+    using EducationPortal.BLL.Interfaces;
+    using EducationPortal.DAL.Repositories;
+    using EducationPortal.Domain.Entities;
+    using Entities;
+
+    public class CourseMaterialSqlService : ICourseMaterialService
+    {
+        private readonly IRepository<CourseMaterial> courseMaterialRepository;
+        private readonly IRepository<Course> courseRepository;
+        private readonly IRepository<Material> materialRepository;
+
+        public CourseMaterialSqlService(
+            IEnumerable<IRepository<CourseMaterial>> courseMatRepository,
+            IEnumerable<IRepository<Material>> materialRepo,
+            IEnumerable<IRepository<Course>> courseRepo
+            )
+        {
+            this.courseMaterialRepository = courseMatRepository.FirstOrDefault(t => t.GetType() == typeof(RepositorySql<CourseMaterial>));
+            this.courseRepository = courseRepo.FirstOrDefault(t => t.GetType() == typeof(RepositorySql<Course>));
+            this.materialRepository = materialRepo.FirstOrDefault(t => t.GetType() == typeof(RepositorySql<Material>));
+        }
+
+        public bool AddMaterialToCourse(int courseId, int materialId)
+        {
+            if (this.courseRepository.Exist(x => x.Id == courseId) && this.materialRepository.Exist(x => x.Id == materialId))
+            {
+                CourseMaterial courseMaterial = new CourseMaterial()
+                {
+                    CourseId = courseId,
+                    MaterialId = materialId,
+                };
+
+                this.courseMaterialRepository.Add(courseMaterial);
+                this.courseMaterialRepository.Save();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ BusinessLogicLayer/ServicesSql/CourseSkillSqlService.cs
@@ -0,0 +1,50 @@
+﻿namespace EducationPortal.BLL.ServicesSql
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using DataAccessLayer.Entities;
+    using DataAccessLayer.Interfaces;
+    using EducationPortal.BLL.Interfaces;
+    using EducationPortal.DAL.Repositories;
+    using EducationPortal.Domain.Entities;
+
+    public class CourseSkillSqlService : ICourseSkillService
+    {
+        private readonly IRepository<CourseSkill> courseSkillRepository;
+        private readonly IRepository<Course> courseRepository;
+        private readonly IRepository<Skill> skillRepository;
+
+        public CourseSkillSqlService(
+            IEnumerable<IRepository<CourseSkill>> courseSkillRepo,
+            IEnumerable<IRepository<Skill>> skillRepo,
+            IEnumerable<IRepository<Course>> courseRepo
+            )
+        {
+            this.courseSkillRepository = courseSkillRepo.FirstOrDefault(t => t.GetType() == typeof(RepositorySql<CourseSkill>));
+            this.courseRepository = courseRepo.FirstOrDefault(t => t.GetType() == typeof(RepositorySql<Course>));
+            this.skillRepository = skillRepo.FirstOrDefault(t => t.GetType() == typeof(RepositorySql<Skill>));
+        }
+
+        public bool AddSkillToCourse(int courseId, int skillId)
+        {
+            if (this.courseRepository.Exist(x => x.Id == courseId) && this.skillRepository.Exist(x => x.Id == skillId))
+            {
+                CourseSkill courseSkill = new CourseSkill()
+                {
+                    CourseId = courseId,
+                    SkillId = skillId,
+                };
+
+                this.courseSkillRepository.Add(courseSkill);
+                this.courseSkillRepository.Save();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
