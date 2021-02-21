164a0f0ffabc152dab625913ce2fb0f4d7e05db7
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-21 22:11:44 +0200

    create userCOurseMaterial service

---
+++ BusinessLogicLayer/Interfaces/IUserCourseMaterialSqlService.cs
@@ -0,0 +1,16 @@
+﻿namespace EducationPortal.BLL.Interfaces
+{
+    using Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public interface IUserCourseMaterialSqlService
+    {
+        bool AddMaterialsToUserCourse(int userCourseId, int courseId);
+
+        bool SetPassToMaterial(int userCourseId, int materialId);
+
+        List<Material> GetNotPassedMaterialsFromCourseInProgress(int userCourseId);
+    }
+}
---
+++ BusinessLogicLayer/ServicesSql/UserCourseMaterialSqlService.cs
@@ -0,0 +1,74 @@
+﻿namespace EducationPortal.BLL.ServicesSql
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BusinessLogicLayer.Interfaces;
+    using DataAccessLayer.Entities;
+    using DataAccessLayer.Interfaces;
+    using EducationPortal.BLL.Interfaces;
+    using EducationPortal.DAL.Repositories;
+    using EducationPortal.Domain.Entities;
+    using Entities;
+
+    public class UserCourseMaterialSqlService : IUserCourseMaterialSqlService
+    {
+        private readonly IRepository<UserCourseMaterial> userCourseMaterialRepository;
+        private ICourseMaterialService courseMaterialService;
+
+        public UserCourseMaterialSqlService(
+            IEnumerable<IRepository<UserCourseMaterial>> userCourseMaterialRepository,
+            ICourseMaterialService courseMaterialService)
+        {
+            this.userCourseMaterialRepository = userCourseMaterialRepository.FirstOrDefault(t => t.GetType() == typeof(RepositorySql<UserCourseMaterial>));
+            this.courseMaterialService = courseMaterialService;
+        }
+
+        public bool AddMaterialsToUserCourse(int userCourseId, int courseId)
+        {
+            List<Material> materialsFromCourse = this.courseMaterialService.GetAllMaterialsFromCourse(courseId);
+
+            foreach (var material in materialsFromCourse)
+            {
+                UserCourseMaterial userCourseMaterial = new UserCourseMaterial()
+                {
+                    UserCourseId = userCourseId,
+                    MaterialId = material.Id,
+                    IsPassed = false,
+                };
+
+                this.userCourseMaterialRepository.Add(userCourseMaterial);
+                this.userCourseMaterialRepository.Save();
+            }
+
+            return true;
+        }
+
+        public bool SetPassToMaterial(int userCourseId, int materialId)
+        {
+            UserCourseMaterial userCourseMaterialToUpdate = this.userCourseMaterialRepository.Get(
+                x => x.UserCourseId == userCourseId && x.MaterialId == materialId).FirstOrDefault();
+
+            if (userCourseMaterialToUpdate != null)
+            {
+                userCourseMaterialToUpdate.IsPassed = true;
+                this.userCourseMaterialRepository.Update(userCourseMaterialToUpdate);
+                this.userCourseMaterialRepository.Save();
+                return true;
+            }
+
+            return false;
+        }
+
+        public List<Material> GetNotPassedMaterialsFromCourseInProgress(int userCourseId)
+        {
+            if (!this.userCourseMaterialRepository.Exist(x => x.UserCourseId == userCourseId))
+            {
+                return null;
+            }
+
+            return this.userCourseMaterialRepository.Get<Material>(x => x.Material, x => x.UserCourseId == userCourseId && x.IsPassed == false).ToList();
+        }
+    }
+}
