8910a16fcd6dac2498c2136320f19359f9c31965
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-21 22:12:48 +0200

    Create userSkill service

---
+++ BusinessLogicLayer/Interfaces/IUserSkillSqlService.cs
@@ -0,0 +1,14 @@
+﻿namespace EducationPortal.BLL.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using DataAccessLayer.Entities;
+
+    public interface IUserSkillSqlService
+    {
+        void AddSkillToUser(int userId, int skillId);
+
+        List<Skill> GetAllSkillInUser(int userId);
+    }
+}
---
+++ BusinessLogicLayer/ServicesSql/UserSkillSqlService.cs
@@ -0,0 +1,53 @@
+﻿namespace EducationPortal.BLL.ServicesSql
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using DataAccessLayer.Entities;
+    using DataAccessLayer.Interfaces;
+    using EducationPortal.BLL.Interfaces;
+    using EducationPortal.DAL.Repositories;
+    using EducationPortal.Domain.Entities;
+
+    public class UserSkillSqlService : IUserSkillSqlService
+    {
+        private readonly IRepository<UserSkill> userSkillRepository;
+
+        public UserSkillSqlService(
+            IEnumerable<IRepository<UserSkill>> userSkillRepository)
+        {
+            this.userSkillRepository = userSkillRepository.FirstOrDefault(t => t.GetType() == typeof(RepositorySql<UserSkill>));
+        }
+
+        public void AddSkillToUser(int userId, int skillId)
+        {
+            UserSkill userSkill = this.userSkillRepository.Get(x => x.UserId == userId && x.SkillId == skillId).FirstOrDefault();
+
+            if (userSkill != null)
+            {
+                userSkill.CountOfPoint++;
+                this.userSkillRepository.Update(userSkill);
+            }
+            else
+            {
+                userSkill = new UserSkill()
+                {
+                    UserId = userId,
+                    SkillId = skillId,
+                };
+
+                this.userSkillRepository.Add(userSkill);
+            }
+
+            this.userSkillRepository.Save();
+        }
+
+        public List<Skill> GetAllSkillInUser(int userId)
+        {
+            var s = this.userSkillRepository.Get<Skill>(x => x.Skill, x => x.UserId == userId).ToList();
+            s.
+            return this.userSkillRepository.Get<Skill>(x => x.Skill, x => x.UserId == userId).ToList();
+        }
+    }
+}
