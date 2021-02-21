8c61ab769e28c73ba0d87e06536adf56a6114c8b
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-21 22:08:33 +0200

    Create course comparer

---
+++ BusinessLogicLayer/Interfaces/ICourseComparerService.cs
@@ -0,0 +1,12 @@
+﻿using EducationPortal.Domain.Comparers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationPortal.BLL.Interfaces
+{
+    public interface ICourseComparerService
+    {
+        CourseComparer CourseComparer { get; }
+    }
+}
---
+++ BusinessLogicLayer/ServicesSql/CourseComparerService.cs
@@ -0,0 +1,27 @@
+﻿namespace EducationPortal.BLL.ServicesSql
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using EducationPortal.BLL.Interfaces;
+    using EducationPortal.Domain.Comparers;
+
+    public class CourseComparerService : ICourseComparerService
+    {
+        private CourseComparer courseComparer;
+
+        public CourseComparer CourseComparer
+        {
+            get
+            {
+                if (courseComparer == null)
+                {
+                    courseComparer = new CourseComparer();
+                    return courseComparer;
+                }
+
+                return courseComparer;
+            }
+        }
+    }
+}
