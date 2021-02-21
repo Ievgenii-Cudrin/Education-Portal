b6a6ec6cb527fe208d55d7934698e4ac32dc9046
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-21 22:09:45 +0200

    Create material comparer service

---
+++ BusinessLogicLayer/Interfaces/IMaterialComparerService.cs
@@ -0,0 +1,12 @@
+﻿namespace EducationPortal.BLL.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using EducationPortal.Domain.Comparers;
+
+    public interface IMaterialComparerService
+    {
+        MaterialComparer MaterialComparer { get; }
+    }
+}
---
+++ EducationPortal.Domain/Comparers/MaterialComparer.cs
@@ -0,0 +1,26 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace EducationPortal.Domain.Comparers
+{
+    public class MaterialComparer : IEqualityComparer<Material>
+    {
+        public bool Equals([AllowNull] Material x, [AllowNull] Material y)
+        {
+            if (x != null && y != null)
+            {
+                return x.Id == y.Id;
+            }
+
+            return false;
+        }
+
+        public int GetHashCode([DisallowNull] Material obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
+}
