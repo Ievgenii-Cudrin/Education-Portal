e67645aeec528b3a8e3f149f6b76272430462470
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-24 16:34:47 +0200

    Create Tests

---
+++ EducationPortal.BLL.Tests/ServicesSql/CourseMaterialSqlServiceTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using EducationPortal.DAL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccessLayer.Interfaces;
+using EducationPortal.Domain.Entities;
+using EducationPortal.BLL.Services;
+using System.Linq.Expressions;
+using DataAccessLayer.Entities;
+using Entities;
+
+namespace EducationPortal.BLL.Tests.ServicesSql
+{
+    [TestClass]
+    public class CourseMaterialSqlServiceTest
+    {
+        [TestMethod]
+
+        public void AddMaterialToCourse_CourseMaterialExist_False()
+        {
+            Mock<IRepository<CourseMaterial>> courseMaterialRepo = new Mock<IRepository<CourseMaterial>>();
+            courseMaterialRepo.Setup(db => db.Exist(It.IsAny<Expression<Func<CourseMaterial, bool>>>())).Returns(true);
+
+            CourseMaterialSqlService courseMatService = new CourseMaterialSqlService(courseMaterialRepo.Object);
+
+            Assert.IsFalse(courseMatService.AddMaterialToCourse(2, 3));
+        }
+
+        [TestMethod]
+        public void AddMaterialToCourse_CourseMaterialNotExist_True()
+        {
+            Mock<IRepository<CourseMaterial>> courseMaterialRepo = new Mock<IRepository<CourseMaterial>>();
+            courseMaterialRepo.Setup(db => db.Exist(It.IsAny<Expression<Func<CourseMaterial, bool>>>())).Returns(false);
+            courseMaterialRepo.Setup(db => db.Add(It.IsAny<CourseMaterial>()));
+            courseMaterialRepo.Setup(db => db.Save());
+
+            CourseMaterialSqlService courseMatService = new CourseMaterialSqlService(courseMaterialRepo.Object);
+            CourseMaterial courseMaterial = new CourseMaterial()
+            {
+                CourseId = 2,
+                MaterialId = 3
+            };
+            courseMatService.AddMaterialToCourse(2, 3);
+
+            courseMaterialRepo.Verify(x => x.Save(), Times.Once);
+            Assert.IsTrue(courseMatService.AddMaterialToCourse(2, 3));
+        }
+
+    }
+}
