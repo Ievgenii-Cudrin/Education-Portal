4c2fd1f001936c25372c6b2c3b8ff84cfe3ad0ed
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-27 15:23:32 +0200

    Update Mapper types name

--- EducationPortalConsoleApp/Mapper/Mapper.cs
+++ EducationPortalConsoleApp/Mapper/Mapper.cs
@@ -1,94 +1,97 @@
 ﻿namespace EducationPortal.PL.Mapping
 {
-    using System.Collections.Generic;
     using AutoMapper;
     using EducationPortal.PL.Interfaces;
+    using System.Collections.Generic;
 
     public class Mapping : IMapperService
     {
-        public TN CreateMapFromVMToDomain<T, TN>(T viewModelType)
+        public TDomain CreateMapFromVMToDomain<TView, TDomain>(TView viewModelType)
         {
-            var config = new MapperConfiguration(cfg => cfg.CreateMap<T, TN>());
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<TView, TDomain>());
             var mapper = new Mapper(config);
-            var domainAfterMapping = mapper.Map<T, TN>(viewModelType);
+            var domainAfterMapping = mapper.Map<TView, TDomain>(viewModelType);
 
             return domainAfterMapping;
         }
 
-        public TN CreateMapFromVMToDomainWithIncludeVideoType<T, TN, TIT, TIN>(T viewModelType)
-            where TIT : T
-            where TIN : TN
+        public TDomain CreateMapFromVMToDomainWithIncludeVideoType<TView, TDomain, TViewInclude, TDomainInclude>(TView viewModelType)
+            where TViewInclude : TView
+            where TDomainInclude : TDomain
         {
             var configuration = new MapperConfiguration(cfg =>
             {
-                cfg.CreateMap<T, TN>()
-                    .Include<TIT, TIN>();
-                cfg.CreateMap<TIT, TIN>();
+                cfg.CreateMap<TView, TDomain>()
+                    .Include<TViewInclude, TDomainInclude>();
+                cfg.CreateMap<TViewInclude, TDomainInclude>();
             });
             var mapper = new Mapper(configuration);
-            var domainAfterMapping = mapper.Map<T, TN>(viewModelType);
+            var domainAfterMapping = mapper.Map<TView, TDomain>(viewModelType);
 
             return domainAfterMapping;
         }
 
-        public List<TN> CreateListMap<T, TN>(List<T> list)
+        public List<Domain> CreateListMap<View, Domain>(List<View> list)
         {
-            var configuration = new MapperConfiguration(cfg => cfg.CreateMap<T, TN>());
+            var configuration = new MapperConfiguration(cfg => cfg.CreateMap<View, Domain>());
             var mapper = new Mapper(configuration);
-            List<TN> listDest = mapper.Map<List<T>, List<TN>>(list);
+            List<Domain> listDest = mapper.Map<List<View>, List<Domain>>(list);
 
             return listDest;
         }
 
-        public List<TMV> CreateListMapFromVMToDomainWithIncludeMaterialType<TMD, TMV, TVD, TVV, TAD, TAV, TBD, TBV>(List<TMD> viewModelType)
-            where TVD : TMD
-            where TAD : TMD
-            where TBD : TMD
-            where TVV : TMV
-            where TAV : TMV
-            where TBV : TMV
+        public List<TMaterialView> CreateListMapFromVMToDomainWithIncludeMaterialType
+            <TMaterialDomainD, TMaterialView, TVideoDomain, TVideoView, TArticleDomain, TArticleView, TBookDomain, TBookView>(List<TMaterialDomainD> viewModelType)
+            where TVideoDomain : TMaterialDomainD
+            where TArticleDomain : TMaterialDomainD
+            where TBookDomain : TMaterialDomainD
+            where TVideoView : TMaterialView
+            where TArticleView : TMaterialView
+            where TBookView : TMaterialView
         {
             var configuration = new MapperConfiguration(cfg =>
             {
-                cfg.CreateMap<TMD, TMV>()
-                    .Include<TVD, TVV>()
-                    .Include<TAD, TAV>()
-                    .Include<TBD, TBV>();
-                cfg.CreateMap<TBD, TBV>();
-                cfg.CreateMap<TVD, TVV>();
-                cfg.CreateMap<TAD, TAV>();
+                cfg.CreateMap<TMaterialDomainD, TMaterialView>()
+                    .Include<TVideoDomain, TVideoView>()
+                    .Include<TArticleDomain, TArticleView>()
+                    .Include<TBookDomain, TBookView>();
+                cfg.CreateMap<TBookDomain, TBookView>();
+                cfg.CreateMap<TVideoDomain, TVideoView>();
+                cfg.CreateMap<TArticleDomain, TArticleView>();
             });
             var mapper = new Mapper(configuration);
-            var domainAfterMapping = mapper.Map<List<TMD>, List<TMV>>(viewModelType);
+            var domainAfterMapping = mapper.Map<List<TMaterialDomainD>, List<TMaterialView>>(viewModelType);
 
             return domainAfterMapping;
         }
 
-        public List<TCV> CreateListMapFromVMToDomainWithIncludeLsitType<TCD, TCV, TMD, TMV, TSD, TSV>(List<TCD> viewModelType)
+        public List<TCourseView> CreateListMapFromVMToDomainWithIncludeLsitType
+            <TCourseDomain, TCourseView, TMaterialDomain, TMaterialView, TSkillDomain, TSkillView>(List<TCourseDomain> viewModelType)
         {
             var configuration = new MapperConfiguration(cfg =>
             {
-                cfg.CreateMap<TCD, TCV>();
-                cfg.CreateMap<List<TMD>, List<TMV>>();
-                cfg.CreateMap<List<TSD>, List<TSV>>();
+                cfg.CreateMap<TCourseDomain, TCourseView>();
+                cfg.CreateMap<List<TMaterialDomain>, List<TMaterialView>>();
+                cfg.CreateMap<List<TSkillDomain>, List<TSkillView>>();
             });
 
             var mapper = new Mapper(configuration);
-            var domainAfterMapping = mapper.Map<List<TCD>, List<TCV>>(viewModelType);
+            var domainAfterMapping = mapper.Map<List<TCourseDomain>, List<TCourseView>>(viewModelType);
 
             return domainAfterMapping;
         }
 
-        public TCV CreateMapFromVMToDomainWithIncludeLsitType<TCD, TCV, TMD, TMV, TSD, TSV>(TCD viewModelType)
+        public TCourseView CreateMapFromVMToDomainWithIncludeLsitType
+            <TCourseDomain, TCourseView, TModelDomain, TModelView, TSkillDomain, TSkillView>(TCourseDomain viewModelType)
         {
             var configuration = new MapperConfiguration(cfg =>
             {
-                cfg.CreateMap<TCD, TCV>();
-                cfg.CreateMap<List<TMD>, List<TMV>>();
-                cfg.CreateMap<List<TSD>, List<TSV>>();
+                cfg.CreateMap<TCourseDomain, TCourseView>();
+                cfg.CreateMap<List<TModelDomain>, List<TModelView>>();
+                cfg.CreateMap<List<TSkillDomain>, List<TSkillView>>();
             });
             var mapper = new Mapper(configuration);
-            var domainAfterMapping = mapper.Map<TCD, TCV>(viewModelType);
+            var domainAfterMapping = mapper.Map<TCourseDomain, TCourseView>(viewModelType);
 
             return domainAfterMapping;
         }
