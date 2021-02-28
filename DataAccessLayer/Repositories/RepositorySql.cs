51c563ead34f44181d11e9a0ce0ef258493a4046
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-02-28 18:12:48 +0200

    Add logger to sql repository

--- DataAccessLayer/Repositories/RepositorySql.cs
+++ DataAccessLayer/Repositories/RepositorySql.cs
@@ -7,84 +7,152 @@
     using DataAccessLayer.Entities;
     using DataAccessLayer.Interfaces;
     using EducationPortal.DAL.DataContext;
+    using EducationPortal.DAL.Interfaces;
     using EducationPortal.Domain.Entities;
     using Microsoft.EntityFrameworkCore;
+    using NLog;
 
     public class RepositorySql<T> : IRepository<T>
         where T : class
     {
         private readonly ApplicationContext dbContext;
+        private static IDalSqlLogger logger;
 
-        public RepositorySql(ApplicationContext context)
+        public RepositorySql(IDalSqlLogger log, ApplicationContext context)
         {
+            logger = log;
             this.dbContext = context;
         }
 
         public IList<T> GetAll()
         {
-            return this.dbContext.Set<T>().ToList();
+            try
+            {
+                return this.dbContext.Set<T>().ToList();
+            }
+            catch
+            {
+                logger.Logger.Error("Cant connect to DB - " + DateTime.Now);
+                return null;
+            }
         }
 
         public IList<T> GetAll(params Expression<Func<T, object>>[] includes)
         {
-            var result = this.dbContext.Set<T>();
-            foreach (var include in includes)
+            try
             {
-                result.Include(include);
-            }
+                var result = this.dbContext.Set<T>();
+                foreach (var include in includes)
+                {
+                    result.Include(include);
+                }
 
-            return result.ToList();
+                return result.ToList();
+            }
+            catch
+            {
+                logger.Logger.Error("Cant connect to DB - " + DateTime.Now);
+                return null;
+            }
         }
 
         public IList<T> Get(
             Expression<Func<T, bool>> predicat,
             params Expression<Func<T, object>>[] includes)
         {
-            var result = this.dbContext.Set<T>();
-            foreach (var include in includes)
+            try
             {
-                result.Include(include);
-            }
+                var result = this.dbContext.Set<T>();
+                foreach (var include in includes)
+                {
+                    result.Include(include);
+                }
 
-            return result.Where(predicat).ToList();
+                return result.Where(predicat).ToList();
+            }
+            catch
+            {
+                logger.Logger.Error("Cant connect to DB - " + DateTime.Now);
+                return null;
+            }
         }
 
         public IList<TResult> Get<TResult>(Expression<Func<T, TResult>> selector)
         {
-            return this.dbContext.Set<T>()
+            try
+            {
+                return this.dbContext.Set<T>()
                         .Select(selector).ToList();
+            }
+            catch
+            {
+                logger.Logger.Error("Cant connect to DB - " + DateTime.Now);
+                return null;
+            }
         }
 
         public IList<TResult> Get<TResult>(
             Expression<Func<T, TResult>> selector,
             Expression<Func<T, bool>> predicat)
         {
-            return this.dbContext.Set<T>()
+            try
+            {
+                return this.dbContext.Set<T>()
                         .Where(predicat)
                         .Select(selector).ToList();
+            }
+            catch
+            {
+                logger.Logger.Error("Cant connect to DB - " + DateTime.Now);
+                return null;
+            }
         }
 
         public IList<T> Get(Expression<Func<T, bool>> predicat)
         {
-            return this.dbContext.Set<T>()
+            try
+            {
+                return this.dbContext.Set<T>()
                         .Where(predicat).ToList();
+            }
+            catch
+            {
+                logger.Logger.Error("Cant connect to DB - " + DateTime.Now);
+                return null;
+            }
         }
 
         public IList<T> GetPage(PageInfo page)
         {
-            var skip = page.Size * (page.Number - 1);
-            return this.dbContext.Set<T>()
-                        .Skip(skip).Take(page.Size).ToList();
+            try
+            {
+                var skip = page.Size * (page.Number - 1);
+                return this.dbContext.Set<T>()
+                            .Skip(skip).Take(page.Size).ToList();
+            }
+            catch
+            {
+                logger.Logger.Error("Cant connect to DB - " + DateTime.Now);
+                return null;
+            }
         }
 
         public IList<T> GetPage(
             Expression<Func<T, bool>> predicat,
             PageInfo page)
         {
-            var skip = page.Size * (page.Number - 1);
-            return this.dbContext.Set<T>()
-                        .Where(predicat)
-                        .Skip(skip).Take(page.Size).ToList();
+            try
+            {
+                var skip = page.Size * (page.Number - 1);
+                return this.dbContext.Set<T>()
+                            .Where(predicat)
+                            .Skip(skip).Take(page.Size).ToList();
+            }
+            catch
+            {
+                logger.Logger.Error("Cant connect to DB - " + DateTime.Now);
+                return null;
+            }
         }
 
         // public IList<TEntity> GetPage<TEntity>(
@@ -101,47 +169,107 @@
         // }
         public bool Exist(Expression<Func<T, bool>> predicat)
         {
-            return this.dbContext.Set<T>().Any(predicat);
+            try
+            {
+                return this.dbContext.Set<T>().Any(predicat);
+            }
+            catch
+            {
+                logger.Logger.Error("Cant connect to DB - " + DateTime.Now);
+                return false;
+            }
         }
 
         public void Add(T entity)
         {
-            this.dbContext.Set<T>().Add(entity);
+            try
+            {
+                this.dbContext.Set<T>().Add(entity);
+            }
+            catch
+            {
+                logger.Logger.Error("Cant connect to DB - " + DateTime.Now);
+            }
         }
 
         public void Save()
         {
-            this.dbContext.SaveChanges();
+            try
+            {
+                this.dbContext.SaveChanges();
+            }
+            catch
+            {
+                logger.Logger.Error("Cant connect to DB - " + DateTime.Now);
+            }
         }
 
         public T Get(int id)
         {
-            return this.dbContext.Set<T>().Find(id);
+            try
+            {
+                return this.dbContext.Set<T>().Find(id);
+            }
+            catch
+            {
+                logger.Logger.Error("Cant connect to DB - " + DateTime.Now);
+                return null;
+            }
         }
 
         public void Update(T item)
         {
-            this.dbContext.Entry<T>(item).State = EntityState.Modified;
+            try
+            {
+                this.dbContext.Entry<T>(item).State = EntityState.Modified;
+            }
+            catch
+            {
+                logger.Logger.Error("Cant connect to DB - " + DateTime.Now);
+            }
         }
 
         public void Delete(int id)
         {
-            var entity = this.dbContext.Set<T>().Find(id);
-
-            if (entity != null)
+            try
             {
-                this.dbContext.Set<T>().Remove(entity);
+                var entity = this.dbContext.Set<T>().Find(id);
+
+                if (entity != null)
+                {
+                    this.dbContext.Set<T>().Remove(entity);
+                }
+            }
+            catch
+            {
+                logger.Logger.Error("Cant connect to DB - " + DateTime.Now);
             }
         }
 
         public IList<T> Except(IList<T> list, IEqualityComparer<T> comparer)
         {
-            return this.dbContext.Set<T>().ToList().Except(list, comparer).ToList();
+            try
+            {
+                return this.dbContext.Set<T>().ToList().Except(list, comparer).ToList();
+            }
+            catch
+            {
+                logger.Logger.Error("Cant connect to DB - " + DateTime.Now);
+                return null;
+            }
         }
 
         public T GetLastEntity<TOrderBy>(Expression<Func<T, TOrderBy>> orderBy)
         {
-            return this.dbContext.Set<T>().OrderBy(orderBy).Last();
+            try
+            {
+                return this.dbContext.Set<T>().OrderBy(orderBy).Last();
+            }
+            catch
+            {
+                logger.Logger.Error("Cant connect to DB - " + DateTime.Now);
+                return null;
+            }
         }
     }
 }
