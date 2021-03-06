4e4718f17495a062225df34515bc6d751d6d73c5
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-03-06 15:13:57 +0200

    Add connection string and DI

--- EducationPartal.CoreMVC/Startup.cs
+++ EducationPartal.CoreMVC/Startup.cs
@@ -1,6 +1,20 @@
+using BusinessLogicLayer.Interfaces;
+using DataAccessLayer.DataContext;
+using DataAccessLayer.Interfaces;
+using EducationPortal.BLL.Interfaces;
+using EducationPortal.BLL.Loggers;
+using EducationPortal.BLL.Services;
+using EducationPortal.BLL.ServicesSql;
+using EducationPortal.DAL.DataContext;
+using EducationPortal.DAL.Interfaces;
+using EducationPortal.DAL.Loggers;
+using EducationPortal.DAL.Repositories;
+using EducationPortal.DAL.XML.Repositories;
+using EducationPortal.Domain.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +22,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using XmlDataBase.Interfaces;
+using XmlDataBase.Serialization;
 
 namespace EducationPartal.CoreMVC
 {
@@ -20,13 +36,45 @@ namespace EducationPartal.CoreMVC
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(typeof(IXmlSet<>), typeof(XmlSet<>));
+            services.AddSingleton(typeof(IXmlSerializeContext<>), typeof(XmlSerializationContextGeneric<>));
+            // Repositories
+            services.AddTransient<IRepository<UserCourse>, UserCourseXmlRepository>();
+            services.AddSingleton<IRepository<CourseMaterial>, CourseMaterialXmlRepository>();
+            services.AddTransient<IRepository<CourseSkill>, CourseSkillXmlRepository>();
+            services.AddTransient<IRepository<UserMaterial>, UserMaterialXmlRepository>();
+            services.AddTransient<IRepository<UserSkill>, UserSkillXmlRepository>();
+            services.AddTransient<IRepository<UserCourseMaterial>, UserCourseMaterialXmlRepository>();
+            services.AddTransient(typeof(IRepository<>), typeof(RepositoryXml<>));
+            //services.AddTransient(typeof(IRepository<>), typeof(RepositorySql<>));
+            // Services
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<ICourseService, CourseService>();
+            services.AddTransient<IMaterialService, MaterialService>();
+            services.AddTransient<ISkillService, SkillService>();
+            services.AddTransient<ILogInService, LogInService>();
+            services.AddScoped<IUserCourseSqlService, UserCourseService>();
+            services.AddTransient<ICourseMaterialService, CourseMaterialService>();
+            services.AddTransient<ICourseSkillService, CourseSkillService>();
+            services.AddTransient<IUserCourseMaterialSqlService, UserCourseMaterialService>();
+            services.AddTransient<IUserMaterialSqlService, UserMaterialService>();
+            services.AddTransient<IUserSkillSqlService, UserSkillService>();
+            services.AddTransient<IMaterialComparerService, MaterialComparerService>();
+            services.AddTransient<ICourseComparerService, CourseComparerService>();
+            services.AddTransient<IAuthorizedUser, AuthorizerUser>();
+            services.AddTransient<IWorkWithAuthorizedUser, AuthorizerUser>();
+            //Loggers
+            services.AddTransient<IDalSqlLogger, DalSqlNLogLogger>();
+            services.AddTransient<IBLLLogger, BLLNlogLogger>();
+
+            services.AddDbContext<ApplicationContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
