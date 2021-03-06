53f6c11a93f4b1c380a74648dccf7fde48ee4f62
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-03-06 20:39:46 +0200

    Create view models

---
+++ EducationPartal.CoreMVC/ModelsView/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducationPartal.CoreMVC.ModelsView
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Не указано имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароль введен неверно")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Неверный формат номера")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Не неверный Email")]
+        public string Email { get; set; }
+    }
+}
