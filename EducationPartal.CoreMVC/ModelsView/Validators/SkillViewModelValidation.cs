2ed3c20fe4bbd71f5d0aadea1571073796c08933
Ievgenii-Cudrin <ievgeniicudrin@gmail.com>
2021-03-11 22:53:04 +0200

    create validators

---
+++ EducationPartal.CoreMVC/ModelsView/Validators/SkillViewModelValidation.cs
@@ -0,0 +1,21 @@
+﻿using EducationPartal.CoreMVC.ModelsView;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducationPortal.CoreMVC.ModelsView.Validators
+{
+    public class SkillViewModelValidation : AbstractValidator<CourseViewModel>
+    {
+        public SkillViewModelValidation()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MinimumLength(2)
+                .MaximumLength(100)
+                .WithMessage("Incorrect name length. Name length must be from 2 to 100 chars.");
+        }
+    }
+}
